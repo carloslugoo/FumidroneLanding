7ba2a0ab9d86926c048d4fe608842e77e831629d
Carlos Lugo <lugostein201@gmail.com>
2024-03-11 02:24:08 -0300

    Big update

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,3 +6,65 @@ window.addEventListener('load', ()=>{
     spinnerWrapperElement.style.display='none';
     },200)
 });
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.querySelectorAll('.element');
+
+    var options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5 // Porcentaje del elemento visible para activar la animación
+    };
+
+    var observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    elements.forEach(function(element) {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var counters = document.querySelectorAll('.counter');
+
+    var options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    var observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                startCounter(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    counters.forEach(function (counter) {
+        observer.observe(counter);
+    });
+
+    function startCounter(counter) {
+        var target = parseInt(counter.getAttribute('data-target'));
+        var current = 0;
+        if (target > 60){
+            current = 60
+        }
+        var interval = setInterval(function () {
+
+            counter.textContent = current + "+";
+            current++;
+
+            if (current > target) {
+                clearInterval(interval);
+            }
+        }, 50); // Ajusta la velocidad del contador según tus preferencias
+    }
+});
